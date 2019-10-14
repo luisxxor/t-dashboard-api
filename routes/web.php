cc62769277bad7f287e9ea7041d9497e88b551ce
Luis Portillo <luisxxor@gmail.com>
2019-10-14 14:39:59 -0400

    Trying to fix

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,7 @@
 |
 */
 
-Route::get( 'vue-app/{any}', function() {
-    return view('index');
-})->where( 'any','.*');
-
 Route::get( 'logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index' );
 
 Route::get('login/{provider}', 'SocialController@redirect');
 Route::get('login/{provider}/callback','SocialController@Callback');
-
-// Download files
-Route::name( 'downloadFiles' )
-    ->get(
-        '/download_files/{fileName}',
-        function ( $fileName ) {
-            return response()->download( sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName );
-        }
-    )
-    ->where( [ 'file' => '(.*?)\.(xls|xlsx|csv|pdf)$' ] );
