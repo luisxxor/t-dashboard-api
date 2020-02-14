f350457f550e8e475e821426e6f46c0319769977
kaorux <Kaidot123>
2020-02-13 22:11:22 -0400

    small fixed bucket

--- app/Projects/ChileProperties/Controllers/PropertiesAPIController.php
+++ app/Projects/ChileProperties/Controllers/PropertiesAPIController.php
@@ -702,7 +702,8 @@ class PropertiesAPIController extends AppBaseController
                 array_merge( $search->toArray(), [ 'data' => $selectedSearchedProperties ] ),
                 $rowsQuantity,
                 $orderCode,
-                'json'
+                'json',
+                config( 'app.cl_export_file_bucket' )
             );
 
             // free memory
@@ -723,7 +724,8 @@ class PropertiesAPIController extends AppBaseController
                 ],
                 $rowsQuantity,
                 $orderCode,
-                'xlsx'
+                'xlsx',
+                config( 'app.cl_export_file_bucket' )
             );
 
             gc_collect_cycles();
